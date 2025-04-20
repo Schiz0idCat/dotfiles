cfed69c3afbec87d86035fe13bace709319076f0
Schiz0idCat <agustin.guzman.v@proton.me>
2025-04-20 09:59:39 -0400

    lsp c language

---
+++ nvim/configs/lspconfig.lua
@@ -0,0 +1,15 @@
+local nvlsp = require "nvchad.configs.lspconfig"
+local lspconfig = require "lspconfig"
+
+nvlsp.defaults()
+
+local servers = { "clangd" }
+vim.lsp.enable(servers)
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
