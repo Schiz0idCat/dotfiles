31ca339a41ed8e77f9ac537312f70637cead957a
Schiz0idCat <agustin.guzman.v@proton.me>
2025-04-20 20:38:18 -0400

    types and max

--- nvim/plugins/cmp.lua
+++ nvim/plugins/cmp.lua
@@ -4,6 +4,7 @@ return {
         opts.sources = {
             {
                 name = "nvim_lsp", -- only LSP autocompletion
+                max_item_count = 5, -- max suggestions
                 entry_filter = function (entry, _)
                     local kind = require("cmp.types").lsp.CompletionItemKind[entry:get_kind()]
                     local allowed_kinds = { -- labels kind that I want to see
@@ -14,6 +15,8 @@ return {
                         Type = true,
                         Keyword = true,
                         Snippet = true,
+                        Constant = true,
+                        Class = true,
                         -- Folder = true, -- it doesn't works btw
                         -- File = true, -- it doesn't works btw
                     }
@@ -21,7 +24,11 @@ return {
                 end,
             },
 
-            { name = "path" } -- recover path suggestions
+            {
+                name = "path",  -- recovering path suggestions
+                max_item_count = 5, -- max suggestions
+            },
+
         }
         return opts
     end,
