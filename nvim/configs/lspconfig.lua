bde7abf6f9455afb016a8903d6a5885bf58b1461
Schiz0idCat <agustin.guzman.v@proton.me>
2025-04-21 16:23:42 -0400

    multiple lsp instace at a time bug resolved

--- nvim/configs/lspconfig.lua
+++ nvim/configs/lspconfig.lua
@@ -13,3 +13,17 @@ for _, lsp in ipairs(servers) do
         capabilities = nvlsp.capabilities,
     }
 end
+
+vim.api.nvim_create_autocmd("LspAttach", { -- avoid more than one lsp instance at a time
+    callback = function(args)
+        local clients = vim.lsp.get_clients({ bufnr = args.buf })
+        local seen = {}
+        for _, client in ipairs(clients) do
+            if seen[client.name] then
+                vim.lsp.stop_client(client.id)
+            else
+                seen[client.name] = true
+            end
+        end
+    end,
+})
