3eb43efdfd5fbe0f872d7b5f08c95c47afd830ea
Schiz0idCat <agustin.guzman.v@proton.me>
2025-04-23 15:59:56 -0400

    aestethic :)

--- install.sh
+++ install.sh
@@ -1,189 +1,187 @@
 #!/bin/bash
 
-sudo apt update
-
-# Instalar nodejs y npm
-if ! command -v nodejs &>/dev/null || ! command -v npm &>/dev/null; then
-    echo "Instalando nodejs y npm..."
-    sudo apt install -y nodejs npm || {
-        echo "Falló la instalación de nodejs o npm. Abortando."
-        exit 1
-    }
+if ! sudo apt update; then
+  echo "Incorrect password"
+  exit 1
 fi
-echo "nodejs y npm ya están instalados."
 
+echo ""
 
-# Instalar bash-language-server
-if ! command -v bash-language-server &>/dev/null; then
-    echo "Instalando bash-language-server..."
-    npm i -g bash-language-server|| {
-        echo "Falló la instalación de bash-language-server. Abortando."
-        exit 1
-    }
-fi
-echo "bash-language-server ya está instalado."
-
-##### TERMINAL #####
-# instalar zsh
-if ! command -v zsh &> /dev/null; then
-    echo "Instalando zsh..."
-    sudo apt install -y zsh || {
-        echo "Falló la instalación de zsh. Abortando."
-        exit 1
-    }
-fi
-echo -e "zsh instalado en el sistema\n"
-
-# font
+##### MEDIA #####
+# Font
 FONT="JetBrainsMono"
 DEST="$HOME/.local/share/fonts"
 
-if fc-list | grep -qi "$FONT"; then
-    echo -e "Fuente $FONT instalada en el sistema.\n"
-else
-    mkdir -p "$DEST"
+if ! fc-list | grep -qi "$FONT"; then
+    echo "Installing $FONT..."
 
-    pushd "$DEST" > /dev/null || {
-        echo "Error: no se pudo acceder al directorio de destino."
+    mkdir -p "$DEST" || {
+        echo "Error: failed to create destination directory: $DEST"
         exit 1
     }
 
-    echo "Descargando $FONT Nerd Font..."
-    curl -fLo "${FONT}.zip" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${FONT}.zip"
+    pushd "$DEST" > /dev/null || {
+        echo "Error: failed to access destination directory."
+        exit 1
+    }
 
-    if [ $? -ne 0 ]; then
-        echo "Error al descargar la fuente. Abortando instalación."
+    echo "Downloading $FONT Nerd Font..."
+    if ! curl -fLo "${FONT}.zip" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${FONT}.zip"; then
+        echo "Error: failed to download the font. Aborting installation."
         popd > /dev/null
         exit 1
-    else
-        unzip -o "${FONT}.zip"
-        rm "${FONT}.zip"
-
-        echo "Recargando caché de fuentes..."
-        fc-cache -fv
-
-        echo -e "Fuente $FONT instalada en el sistema.\n"
     fi
+
+    unzip -o "${FONT}.zip"
+    rm "${FONT}.zip"
+
+    echo "Refreshing font cache..."
+    fc-cache -fv
 
     popd > /dev/null
 fi
-
-# instalar oh my zsh
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo "Instalando Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-    if [ $? -ne 0 ]; then
-        echo "Error al instalar Oh My Zsh. Abortando."
-        exit 1
-    fi
-fi
-echo -e "Oh My Zsh instalado en el sistema\n"
-
-# plugins
-# syntax highlighting
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-    echo "Instalando zsh-syntax-highlighting..."
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-
-    if [ $? -ne 0 ]; then
-        echo "Error al clonar zsh-syntax-highlighting. Abortando."
-        exit 1
-    fi
-fi
-echo "zsh-syntax-highlighting ya está instalado."
-
-# autosuggestions
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-    echo "Instalando zsh-autosuggestions..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions \
-        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-
-    if [ $? -ne 0 ]; then
-        echo "Error al clonar zsh-autosuggestions. Abortando."
-        exit 1
-    fi
-fi
-echo "zsh-autosuggestions ya está instalado."
+echo "Font $FONT installed."
 
 echo ""
 
-# instalar powerlevel10k
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-    echo "Instalando Powerlevel10k..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
-        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
-    if [ $? -ne 0 ]; then
-        echo "Error al clonar Powerlevel10k. Abortando."
-        exit 1
-    fi
-fi
-echo -e "Powerlevel10k instalado en el sistema.\n"
-
-echo ""
-
-# comandos extra
-# instalar bat
-if ! command -v bat >/dev/null 2>&1 && ! command -v batcat >/dev/null 2>&1; then
-    echo "Instalando bat..."
-    sudo apt install -y bat
-
-    if [ $? -ne 0 ]; then
-        echo "Error al instalar bat. Abortando."
-        exit 1
-    fi
-fi
-echo "bat instalado en el sistema"
-
-# instalar lsd
-if ! command -v lsd >/dev/null 2>&1; then
-    echo "Instalando lsd..."
-    sudo apt install -y lsd
-
-    if [ $? -ne 0 ]; then
-        echo "Error al instalar lsd. Abortando."
-        exit 1
-    fi
-fi
-echo "lsd instalado en el sistema"
-
-# instalar Neovim
-if ! command -v nvim &> /dev/null; then
-    echo "Instalando Neovim..."
-
-    TEMP_DIR=$(mktemp -d)
-    pushd "$TEMP_DIR" > /dev/null || {
-        echo "Error al acceder al directorio temporal."
+##### TERMINAL #####
+# Install zsh
+if ! command -v zsh &> /dev/null; then
+    echo "Installing zsh..."
+    sudo apt install -y zsh || {
+        echo "Error: zsh installation failed. Aborting."
         exit 1
     }
+fi
+echo "zsh installed."
 
-    echo "Descargando Neovim..."
-    curl -LO https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz
+# Install Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
+    if ! sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+        echo "Error: failed to install Oh My Zsh. Aborting."
+        exit 1
+    fi
+fi
+echo "Oh My Zsh installed."
 
-    if [ $? -ne 0 ]; then
-        echo "Error al descargar Neovim. Abortando."
-        popd > /dev/null
+# Install Powerlevel10k
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+    echo "Installing Powerlevel10k..."
+    if ! git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"; then
+        echo "Error: failed to clone Powerlevel10k. Aborting."
+        exit 1
+    fi
+fi
+echo "Powerlevel10k installed."
+
+echo ""
+
+# plugins
+# Install syntax highlighting
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh-syntax-highlighting..."
+    if ! git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"; then
+        echo "Error: failed to clone zsh-syntax-highlighting. Aborting."
+        exit 1
+    fi
+fi
+echo "zsh-syntax-highlighting installed."
+
+# Install zsh-autosuggestions
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions..."
+    if ! git clone https://github.com/zsh-users/zsh-autosuggestions \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"; then
+        echo "Error: failed to clone zsh-autosuggestions. Aborting."
+        exit 1
+    fi
+fi
+echo "zsh-autosuggestions installed."
+
+echo ""
+
+# Extra commands
+# Install bat
+if ! command -v batcat >/dev/null 2>&1 && ! command -v batcat >/dev/null 2>&1; then
+    echo "Installing bat..."
+    if ! sudo apt install -y bat; then
+        echo "Error: failed to install bat. Aborting."
+        exit 1
+    fi
+fi
+echo "bat installed."
+
+# Install lsd
+if ! command -v lsd >/dev/null 2>&1; then
+    echo "Installing lsd..."
+    if ! sudo apt install -y lsd; then
+        echo "Error: failed to install lsd. Aborting."
+        exit 1
+    fi
+fi
+echo "lsd installed."
+
+echo ""
+
+# Install Neovim
+if ! command -v nvim >/dev/null 2>&1; then
+    echo "Installing Neovim..."
+
+    TEMP_DIR=$(mktemp -d)
+    if ! pushd "$TEMP_DIR" >/dev/null; then
+        echo "Error: failed to access temporary directory. Aborting."
         exit 1
     fi
 
+    echo "Downloading Neovim 0.11..."
+    if ! curl -LO https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz; then
+        echo "Error: failed to download Neovim. Aborting."
+        popd >/dev/null
+        exit 1
+    fi
+
+    echo "Extracting Neovim..."
     tar xzvf nvim-linux-x86_64.tar.gz
 
+    echo "Installing Neovim to /opt/nvim..."
     sudo mv nvim-linux-x86_64 /opt/nvim
     sudo ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
 
-    popd > /dev/null
+    popd >/dev/null
     rm -rf "$TEMP_DIR"
 fi
-echo "Neovim ya está instalado en el sistema."
+echo "Neovim installed."
 
-# Desde este punto se asume que nvchad está instalado
+# Install nvchad
+
+# nvchad extras
+# Install Node.js and npm
+if ! command -v nodejs >/dev/null 2>&1 || ! command -v npm >/dev/null 2>&1; then
+    echo "Installing Node.js and npm..."
+    if ! sudo apt install -y nodejs npm; then
+        echo "Error: failed to install Node.js or npm. Aborting."
+        exit 1
+    fi
+fi
+echo "Node.js and npm installed."
+
+# Install bash-language-server
+if ! command -v bash-language-server >/dev/null 2>&1; then
+    echo "Installing bash-language-server..."
+    if ! npm install -g bash-language-server; then
+        echo "Error: failed to install bash-language-server. Aborting."
+        exit 1
+    fi
+fi
+echo "bash-language-server installed."
 
 if [ ! -d "$HOME/.config/nvim/spell/" ]; then
     mkdir ~/.config/nvim/spell/
 fi
-echo "Directorio de idiomas creado"
+echo "Spell directory created."
 
 echo ""
 
@@ -191,13 +189,13 @@ echo ""
 # .zshrc
 ln -sf ~/dotfiles/terminal/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/terminal/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
-echo "Enlaces simbólicos de zsh creados"
+echo "zsh link created"
 
 # powerlevel10k
 ln -sf ~/dotfiles/terminal/powerlevel10k/.p10k.zsh ~/.p10k.zsh
-echo "Enlace simbólico de powerlevel10k creado"
+echo "powerlevel10k link created"
 
-# nvchad ##### ASUMIENDO QUE NVCHAD ESTÁ INSTALADO #####
+# nvchad
 ln -sf ~/dotfiles/nvim/spell/es.utf-8.spl ~/.config/nvim/spell/es.utf-8.spl
 ln -sf ~/dotfiles/nvim/spell/es.utf-8.sug ~/.config/nvim/spell/es.utf-8.sug
 ln -sf ~/dotfiles/nvim/chadrc.lua ~/.config/nvim/lua/chadrc.lua
@@ -206,9 +204,9 @@ ln -sf ~/dotfiles/nvim/configs/lspconfig.lua ~/.config/nvim/lua/configs/lspconfi
 ln -sf ~/dotfiles/nvim/configs/spell.lua ~/.config/nvim/lua/configs/spell.lua
 ln -sf ~/dotfiles/nvim/plugins/cmp.lua ~/.config/nvim/lua/plugins/cmp.lua
 ln -sf ~/dotfiles/nvim/plugins/nvimtree.lua ~/.config/nvim/lua/plugins/nvimtree.lua
-echo "Enlace simbólico de nvchad creado"
+echo "nvchad link created"
 
 echo ""
 
 ##### FIN #####
-echo "Configuración de dotfiles completada"
+echo "dotfiles setup done :)"
