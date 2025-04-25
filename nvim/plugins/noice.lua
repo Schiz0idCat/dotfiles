9360230db94b63d7280c5e9f717ad2ec295ce5be
Schiz0idCat <agustin.guzman.v@proton.me>
2025-04-24 21:08:29 -0400

    unnecesary curly brackets

--- nvim/plugins/noice.lua
+++ nvim/plugins/noice.lua
@@ -1,19 +1,17 @@
 return {
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "rcarriga/nvim-notify",
-        },
-        config = function()
-            require("noice").setup({
-                lsp = {
-                    signature = {
-                        enabled = false,
-                    },
-                },
-            })
-        end,
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
     },
+    config = function()
+        require("noice").setup({
+            lsp = {
+                signature = {
+                    enabled = false,
+                },
+            },
+        })
+    end,
 }
