f540e610205a0fe64f6c5439d3c043368ea60d7f
Schiz0idCat <agustin.guzman.v@proton.me>
2025-04-27 11:40:42 -0400

    transparency with kitty

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -3,12 +3,13 @@ local M = {}
 
 M.base46 = {
     theme = "onedark",
+    transparency = true,
 }
 
 M.ui = {
     tabufline = {
         order = { "buffers", "tabs", "btns" } -- no treeOffset, cause it's on the right now
-    }
+    },
 }
 
 require("configs.spell");
