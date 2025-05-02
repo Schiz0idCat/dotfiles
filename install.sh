b0a31249e8197937b514e54d2f09feba7121a026
Schiz0idCat <agustin.guzman.v@proton.me>
2025-05-02 14:33:39 -0400

    nvchad

--- install.sh
+++ install.sh
@@ -210,6 +210,18 @@ fi
 echo "Neovim installed."
 
 # Install nvchad
+if [ ! -d "$HOME/.config/nvim" ]; then
+    echo "Cloning NvChad..."
+    if ! git clone https://github.com/NvChad/starter ~/.config/nvim; then
+        echo "Error: failed to clone NvChad. Aborting."
+        exit 1
+    fi
+
+    nvim +qa
+    nvim --headless +"Lazy! sync" +"autocmd User LazyDone ++once lua require('mason-tool-installer').run()" +qa
+    rm -rf ~/.config/nvim/.git
+fi
+echo "NvChad is already installed. Skipping."
 
 # nvchad extras
 # Install Node.js and npm
