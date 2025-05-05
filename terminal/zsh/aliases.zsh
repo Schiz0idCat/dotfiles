b6bcc7bc81f895a2c5d3523a5caf81122c7dab73
Schiz0idCat <agustin.guzman.v@proton.me>
2025-05-05 18:11:16 -0400

    ls -tree aliases

--- terminal/zsh/aliases.zsh
+++ terminal/zsh/aliases.zsh
@@ -7,7 +7,9 @@ alias ll='lsd -l'
 alias la='lsd -a'
 alias lla='lsd -la'
 alias lt='lsd --tree'
+alias ltl='lsd --tree -l'
 alias lta='lsd --tree -a'
+alias ltla='lsd --tree -la'
 
 ##### brave #####
 alias brave='brave-browser'
