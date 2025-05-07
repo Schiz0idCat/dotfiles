deb4f023331185d85814296458513231e47ba774
Schiz0idCat <agustin.guzman.v@proton.me>
2025-05-07 10:03:25 -0400

    no reinstall

--- dependencies/shell/plugins/fzf-tab.sh
+++ dependencies/shell/plugins/fzf-tab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -d "$PLUGIN_DIR" ]; then
-    echo "Cloning fzf-tab plugin..."
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/fzf-tab" ]; then
+    echo "Installing fzf-tab plugin..."
 
     if ! git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab; then
         echo "Error: failed to clone fzf-tab. Aborting."
