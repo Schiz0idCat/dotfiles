1d0b98fab2a77830b0880731469d0fe493c7527a
Schiz0idCat <agustin.guzman.v@proton.me>
2025-05-24 20:43:42 -0400

    indentation

--- nvim/lua/lsp/pyright.lua
+++ nvim/lua/lsp/pyright.lua
@@ -1,13 +1,13 @@
 return {
-  on_attach = function(client, bufnr)
-  end,
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        diagnosticMode = "openFilesOnly",
-        useLibraryCodeForTypes = true,
-      }
-    }
-  },
+    on_attach = function(client, bufnr)
+    end,
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
+                useLibraryCodeForTypes = true,
+            }
+        }
+    },
 }
