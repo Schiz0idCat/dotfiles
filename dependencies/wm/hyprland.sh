692b21bcab53cc6ff0b51c4f0a780441954d33f6
Agustín <agustin.guzman.v@proton.me>
2025-05-28 21:07:35 -0400

    grammatic error

--- dependencies/wm/hyprland.sh
+++ dependencies/wm/hyprland.sh
@@ -3,7 +3,7 @@
 if ! command -v Hyprland >/dev/null 2>&1; then
     echo "Installing Hyprland..."
 
-    if ! sudo pacman -S --noconfirm hyprland; then
+    if ! sudo pacman -S --noconfirm Hyprland; then
         echo "Error: failed to install Hyprland. Aborting."
         exit 1
     fi
