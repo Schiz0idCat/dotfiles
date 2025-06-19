ea93d9097a130803a41b1b05897b37553ea4749f
Agustín <agustin.guzman.v@proton.me>
2025-06-19 09:08:06 -0400

    pacman installation

--- dependencies/social/spotify.sh
+++ dependencies/social/spotify.sh
@@ -3,7 +3,7 @@
 if ! command -v spotify_player >/dev/null 2>&1; then
     echo "Installing Spotify player..."
 
-    if ! cargo install spotify_player --locked; then
+    if ! sudo pacman -S --noconfirm spotify-player; then
         echo "Error: failed to install Spotify player. Aborting."
         exit 1
     fi
