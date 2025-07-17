36c2d9491cb366a48b0ddb8b947f7c0559ef2b0c
Agustín <agustin.guzman.v@proton.me>
2025-07-17 19:02:14 -0400

    Update for compatibility with latest Hyprland version

--- hypr/shaders/saturation.glsl
+++ hypr/shaders/saturation.glsl
@@ -1,14 +1,18 @@
+#version 320 es
+
 precision mediump float;
 
-varying vec2 v_texcoord;
+in vec2 v_texcoord;
 uniform sampler2D tex;
 
-const float SATURATION_FACTOR = 2.0; // The factor must be an explicit float (use 2.0, not 2)
+out vec4 fragColor;
+
+const float SATURATION_FACTOR = 2.0; // The factor must be an explicit float (e.g. 2.0, not 2)
 const vec3 LUMINANCE_WEIGHTS = vec3(0.299, 0.587, 0.114);
 
 void main() {
-    vec4 originalColor = texture2D(tex, v_texcoord);
+    vec4 originalColor = texture(tex, v_texcoord);
     float luminance = dot(originalColor.rgb, LUMINANCE_WEIGHTS);
     vec3 saturatedColor = mix(vec3(luminance), originalColor.rgb, SATURATION_FACTOR);
-    gl_FragColor = vec4(saturatedColor, originalColor.a);
+    fragColor = vec4(saturatedColor, originalColor.a);
 }
