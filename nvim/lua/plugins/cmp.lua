1ab460e30dda39f50f2759b3c6dd24f9cd0327dd
Agustín <agustin.guzman.v@proton.me>
2025-07-21 15:16:47 -0400

    better indent

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -100,11 +100,13 @@ return {
             -- Cmdline setup
             cmp.setup.cmdline(":", {
                 mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = "path" },
-                }, {
-                    { name = "cmdline" },
-                }),
+                sources = cmp.config.sources(
+                    {
+                        { name = "path" },
+                    },
+                    {
+                        { name = "cmdline" },
+                    }),
             })
 
             cmp.setup.cmdline({ "/", "?" }, {
