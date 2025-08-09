e26f4e3c9a36d9aef4e7f5e382ae8829de7de90a
Agustín <agustin.guzman.v@proton.me>
2025-08-08 23:12:30 -0400

    is this you chatgpt???

--- nvim/lua/lsp/jdtls.lua
+++ nvim/lua/lsp/jdtls.lua
@@ -1,4 +1,4 @@
-local java_cmd = vim.fn.exepath("java")  -- esto debe ser java 11 (tu default)
+local java_cmd = vim.fn.exepath("java")
 local jdtls_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = vim.fn.stdpath("data") .. "/jdtls-workspaces/" .. project_name
