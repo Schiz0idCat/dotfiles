f9e0e7e5e198a2b59960034a9e0b16730af35be0
Agustín <agustin.guzman.v@proton.me>
2025-08-09 18:01:34 -0400

    idea callout

--- nvim/lua/plugins/note-taking.lua
+++ nvim/lua/plugins/note-taking.lua
@@ -3,5 +3,11 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    config = function()
+        require('render-markdown').setup({
+            callout = {
+                note = { raw = '[!IDEA]', rendered = '󰛨 Idea', highlight = 'RenderMarkdownSuccess', category = 'personal' },
+            }
+        })
+    end
 }
