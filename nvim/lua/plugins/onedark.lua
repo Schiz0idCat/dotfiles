ee04892d6c5203b3aae11453fcd4c34179f095b4
Agustín <agustin.guzman.v@proton.me>
2025-08-10 00:08:40 -0400

    underline mut rust variables

--- nvim/lua/plugins/onedark.lua
+++ nvim/lua/plugins/onedark.lua
@@ -2,7 +2,11 @@ return {
     "olimorris/onedarkpro.nvim",
     priority = 1000,
     config = function()
+        require("onedarkpro").setup({
+            highlights = {
+                ["@lsp.typemod.variable.mutable.rust"] = { underline = true },
+            },
+        })
         vim.cmd("colorscheme onedark")
     end,
 }
-
