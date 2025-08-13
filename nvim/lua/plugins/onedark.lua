8cb6bf25e94216a3b287f4e6349bce700bbef026
Agustín <agustin.guzman.v@proton.me>
2025-08-13 16:09:34 -0400

    gitsigns colors change

--- nvim/lua/plugins/onedark.lua
+++ nvim/lua/plugins/onedark.lua
@@ -4,7 +4,12 @@ return {
     config = function()
         require("onedarkpro").setup({
             highlights = {
+                -- lenguague: rust
                 ["@lsp.typemod.variable.mutable.rust"] = { underline = true },
+
+                -- plugin: gitsigns
+                GitSignsAdd = { fg = "${green}" },
+                GitSignsChange = { fg = "${yellow}" },
             },
         })
         vim.cmd("colorscheme onedark")
