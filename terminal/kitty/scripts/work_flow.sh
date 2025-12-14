ffded6fc0e3dee887a4283af5d51eb08b3cfcd89
Agustín Guzmán <agustin.guzman.v@proton.me>
2025-12-13 22:03:59 -0300

    work_flow script

---
+++ terminal/kitty/scripts/work_flow.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cwd="$PWD"
+
+kitty @ set-tab-title run
+kitty @ launch --type=tab --title git --cwd="$cwd" --hold sh -c 'lazygit; exec $SHELL'
+kitty @ launch --type=tab --title nvim --cwd="$cwd" --hold nvim
