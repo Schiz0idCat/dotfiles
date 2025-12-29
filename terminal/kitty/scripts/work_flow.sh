0ee497bacc1cf6f3d846db142d0980672877b07e
Agustín Guzmán <agustin.guzman.v@proton.me>
2025-12-29 14:51:41 -0300

    clear output

--- terminal/kitty/scripts/work_flow.sh
+++ terminal/kitty/scripts/work_flow.sh
@@ -5,3 +5,5 @@ cwd="$PWD"
 kitty @ set-tab-title run
 kitty @ launch --type=tab --title git --cwd="$cwd" --hold sh -c 'lazygit; exec $SHELL'
 kitty @ launch --type=tab --title nvim --cwd="$cwd" --hold nvim
+
+clear
